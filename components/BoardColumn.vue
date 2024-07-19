d3ed7fa7acb292c41f80c7e56bf9e348e6213d90
jgambard <jmichel.gambard@gmail.com>
2024-07-19 13:33:50 +0200

    feat: display board

---
+++ components/BoardColumn.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { type BoardColumn } from "~/repositories/boardsRepository";
+
+type BoardColumnProps = {
+  column: BoardColumn;
+};
+const props = defineProps<BoardColumnProps>();
+const { column } = toRefs(props);
+</script>
+
+<template>
+  <section class="flex min-w-36 flex-col gap-4 rounded-sm bg-white px-6 py-4">
+    <h2 class="text-xl font-extrabold uppercase">
+      {{ column.name }}
+    </h2>
+    <ul class="flex min-h-24 flex-col gap-2">
+      <li
+        class="flex flex-col bg-gray-300 p-2"
+        v-for="task in column.tasks"
+        :key="task.id"
+      >
+        <p class="bold text-lg">{{ task.name }}</p>
+        <p class="text-sm">{{ task.description }}</p>
+      </li>
+    </ul>
+  </section>
+</template>
--- pages/index/boards/[boardID].vue
+++ pages/index/boards/[boardID].vue
@@ -1,10 +1,40 @@
 <script setup lang="ts">
+import { safeParse } from "valibot";
+import BoardColumn from "~/components/BoardColumn.vue";
+import { boardIDSchema } from "~/repositories/boardsRepository";
+
 const route = useRoute();
 const id = route.params.boardID;
+if (typeof id !== "string") {
+  throw {
+    statusCode: 400,
+    statusMessage: "You must only provide a board ID",
+    fatal: true,
+  };
+}
+
+const { output: boardID, success } = safeParse(boardIDSchema, id);
+if (!success) {
+  throw {
+    statusCode: 400,
+    statusMessage: "Invalid board ID",
+    fatal: true,
+  };
+}
+
+const boardsStore = useBoardsStore();
+const board = computed(() => boardsStore.boards.find((b) => b.id === boardID));
 </script>
 
 <template>
-  <div>boardID : {{ id }}</div>
+  <section class="flex items-start gap-2">
+    <BoardColumn
+      v-if="board"
+      v-for="column in board.columns"
+      :key="column.id"
+      :column="column"
+      :boardID="board.id"
+    />
+  </section>
 </template>
 
-<style lang="scss" scoped></style>
